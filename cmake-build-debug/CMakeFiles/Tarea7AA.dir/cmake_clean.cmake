536fec4eb981d3bdb3565a911d99c191373da09f
RickyGomez10 <00108517@uca.edu.sv>
2019-10-22 01:11:41 -0600

    Tarea 7 labo

---
+++ cmake-build-debug/CMakeFiles/Tarea7AA.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tarea7AA.dir/main.cpp.obj"
+  "Tarea7AA.pdb"
+  "Tarea7AA.exe"
+  "Tarea7AA.exe.manifest"
+  "libTarea7AA.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tarea7AA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Tarea7AA.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Tarea7AA.
+# This may be replaced when dependencies are built.
